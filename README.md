dd6df0caf07cd3014288f0d23f9319eb2a5b5329
Shintaro Morikawa <sntr92@gmail.com>
2014-06-27 03:16:51 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 英辞郎(Eijiro) Workflow for [Alfred 2](http://www.alfredapp.com/)
+
+Alfred workflow with which you can search words or sentences in an online EN-JA dictionary "英辞郎 on the WEB".
+
+####[DOWNLOAD](https://github.com/morishin/alfred-eijiro-workflow/releases)
+
+## How it works
+#### EN -> JA
+![EN->JA](http://gyazo.com/dc3f5800d4be667c9a1e9bf795c7d9de.gif)
+
+#### JA -> EN
+![JA->EN](http://gyazo.com/a48f99f023c4b0e94f3ab418aa48dc40.gif)
---
+++ eijiro.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once('libs/workflows.php');
+require_once('libs/simple_html_dom.php');
+
+$wf = new Workflows();
+
+// Alfredに入力されたクエリを取得
+$query = $argv[1];
+
+// クエリを用いてサーバへGETリクエスト
+$ch = curl_init("http://eow.alc.co.jp/search?q=".urlencode($query));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+curl_setopt($ch, CURLOPT_USERAGENT, 'eijiro_alfred_workflow');
+$html = curl_exec($ch);
+curl_close($ch);
+
+// HTMLをパース
+$dom = str_get_html($html);
+$item_number = $dom->find('#itemsNumber', 0)->first_child()->plaintext;
+if ($item_number != '0') {
+    // 検索結果が0件でないとき
+    $child = $dom->find('#resultsList', 0)->first_child()->first_child();
+    $int = 1;
+    while ($child) {
+        // 見出し語を取得
+        $title = $child->find('.midashi', 0)->plaintext;
+        // 文頭文末の空白を除去
+        $title = trim($title);
+        // 連続する空白を1つにする
+        $title = preg_replace('/\s+/', ' ', $title);
+        // 説明文を取得
+        $subtitle = $child->children(1)->plaintext;
+        // Alfredの結果リストに追加
+        $wf->result($int.'.'.time(), "$title", "$title", "$subtitle", null);
+        $child = $child->next_sibling();
+        $int++;
+    }
+}
+
+$results = $wf->results();
+if (count( $results ) == 0):
+    // 結果が0件だったとき
+    $wf->result('eijironotfound', "$query", 'No results', 'No results found for '.$query, 'icon.png');
+endif;
+
+echo $wf->toxml();
+
+?>
